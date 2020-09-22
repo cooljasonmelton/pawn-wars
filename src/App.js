d0b283afaad1269e347daf89eab97866dc0ed936
cooljasonmelton <cooljasonmelton>
2020-09-21 23:08:51 -0500

    styling layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+
+// styling
 import './App.css';
+
+// components
 
 const App = () => {
   return (
     <div className="App">
-      {/* logo */}
-      {/* board */}
-      {/* info box */}
 
-    
+      <div className="logo-c">
+        <h1 className="logo-header">PAWN WARS</h1>
+      </div>
+
+      <div className="cfb">
+        <div className="m-container board-c">
+          chess board
+        </div>
+
+        <div className="m-container info-c">
+          info / options 
+          - instructions
+          - who's turn
+          - new game  - starting pieces - start 
+        </div>   
+      </div>
+
+
     </div>
   );
 }
